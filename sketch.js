cf13d52aa0a54494cfcd5593da7ae1012d89f665
annaec27 <annaec27@gmail.com>
2016-10-13 15:07:15 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
-  createCanvas( 1500, 1500);
+  createCanvas( windowWidth, windowHeight);
 }
 
 function draw() {
   ellipse( mouseX , mouseY , 110 , 110 );
-    stroke(3r, 75g, 75b);
+    fill(3, 75, 75);
 }
